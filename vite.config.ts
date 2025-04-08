a9e925b46d11139855ddd7e7f255400025eb9ef6
Justin Warnes <justin.warnes@iongroup.com>
2025-04-08 17:45:30 -0400

    adds ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), basicSsl()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    https: true
+  }
 });
